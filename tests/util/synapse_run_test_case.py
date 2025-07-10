e04a1384b9023dd12f423dd37d7a9689e7c96234
Harrison <55688012+HarrisonBoyleThomas@users.noreply.github.com>
2025-07-10 14:36:56 +0100

    Chore/theodw 1876 ado pipeline (#2)
    * Remove archival github workflo
    
    * Remove unused tests
    
    * Delete unused ADO components
    
    * Add pipeline for deploying infra
    
    * Update condition
    
    * Remove redundant github workflow
    
    * Remove unused ado scripts

--- tests/util/synapse_run_test_case.py
+++
@@ -1,6 +0,0 @@
-from tests.util.notebook_run_test_case import NotebookRunTestCase
-from tests.util.pipeline_run_test_case import PipelineRunTestCase
-
-
-class SynapseRunTestCase(NotebookRunTestCase, PipelineRunTestCase):
-    pass
