233a72fb2309dc39441df3b86fbfa6721cb6651c
stef-solirius <stefania.deligia@solirius.com>
2025-07-28 09:10:23 +0100

    Removed secondary UK West account and references

--- infrastructure/workload-monitoring.tf
+++ infrastructure/workload-monitoring.tf
@@ -54,9 +54,9 @@ module "synapse_monitoring_failover" {
   alert_group_synapse_enabled              = var.alert_group_synapse_enabled
   alert_scope_service_health               = var.alert_scope_service_health
   alert_threshold_data_lake_capacity_bytes = var.alert_threshold_data_lake_capacity_bytes
-  data_lake_account_id                     = module.synapse_data_lake_failover.data_lake_account_id
+  data_lake_account_id                     = module.synapse_data_lake.data_lake_account_id
   function_app_ids                         = local.function_app_ids
-  key_vault_id                             = module.synapse_data_lake_failover.key_vault_id
+  key_vault_id                             = module.synapse_data_lake.key_vault_id
   service_bus_namespace_id                 = module.synapse_ingestion_failover[0].service_bus_namespace_id
   spark_pool_enabled                       = var.spark_pool_enabled
   sql_pool_enabled                         = var.sql_pool_enabled
