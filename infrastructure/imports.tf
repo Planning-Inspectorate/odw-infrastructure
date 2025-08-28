618a5bd1b09c63116b9e5e1078834905f2f13459
frederic jonquieres <fjonquieres83@gmail.com>
2025-08-28 11:18:30 +0100

    Adding permissions

--- infrastructure/imports.tf
+++ infrastructure/imports.tf
@@ -1,3 +1,15 @@
+resource "azurerm_role_assignment" "ado_blob_reader" {
+  scope                = "/subscriptions/ff442a29-fc06-4a13-8e3e-65fd5da513b3/resourceGroups/pins-rg-data-odw-dev-uks/providers/Microsoft.Storage/storageAccounts/pinsstodwdevuks9h80mb"
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = "2a302373-df67-4c1e-91a1-f6301b87f42b"
+}
+
+resource "azurerm_role_assignment" "ado_storage_reader_management_plane" {
+  scope                = "/subscriptions/ff442a29-fc06-4a13-8e3e-65fd5da513b3/resourceGroups/pins-rg-data-odw-dev-uks/providers/Microsoft.Storage/storageAccounts/pinsstodwdevuks9h80mb"
+  role_definition_name = "Reader"
+  principal_id         = "2a302373-df67-4c1e-91a1-f6301b87f42b"
+}
+
 import {
   to = module.synapse_data_lake.azurerm_storage_container.insights_logs_builtinsqlreqsended
   id = "/subscriptions/ff442a29-fc06-4a13-8e3e-65fd5da513b3/resourceGroups/pins-rg-data-odw-dev-uks/providers/Microsoft.Storage/storageAccounts/pinsstodwdevuks9h80mb/blobServices/default/containers/insights-logs-builtinsqlreqsended"
