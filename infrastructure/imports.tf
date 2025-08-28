6c6607f768d4b5751a57df66fa3b8a7f132c1e10
frederic jonquieres <fjonquieres83@gmail.com>
2025-08-28 10:59:44 +0100

    Updating ID format error ( parsing account "pinsstodwdevuks9h80mb/insights-logs-builtinsqlreqsended": expected the account "" to use a domain suffix of "core.windows.net")

--- infrastructure/imports.tf
+++ infrastructure/imports.tf
@@ -1,39 +1,39 @@
 import {
-  to = module.synapse_data_lake.azurerm_storage_container.logging
-  id = "pinsstodwdevuks9h80mb/logging"
+  to = module.synapse_data_lake.azurerm_storage_container.insights_logs_builtinsqlreqsended
+  id = "/subscriptions/ff442a29-fc06-4a13-8e3e-65fd5da513b3/resourceGroups/pins-rg-data-odw-dev-uks/providers/Microsoft.Storage/storageAccounts/pinsstodwdevuks9h80mb/blobServices/default/containers/insights-logs-builtinsqlreqsended"
 }
 
 import {
-  to = module.synapse_data_lake.azurerm_storage_container.insights_logs_builtinsqlreqsended
-  id = "pinsstodwdevuks9h80mb/insights-logs-builtinsqlreqsended"
+  to = module.synapse_data_lake.azurerm_storage_container.logging
+  id = "/subscriptions/ff442a29-fc06-4a13-8e3e-65fd5da513b3/resourceGroups/pins-rg-data-odw-dev-uks/providers/Microsoft.Storage/storageAccounts/pinsstodwdevuks9h80mb/blobServices/default/containers/logging"
 }
 
 import {
   to = module.synapse_data_lake.azurerm_storage_container.odw_config_db
-  id = "pinsstodwdevuks9h80mb/odw-config-db"
+  id = "/subscriptions/ff442a29-fc06-4a13-8e3e-65fd5da513b3/resourceGroups/pins-rg-data-odw-dev-uks/providers/Microsoft.Storage/storageAccounts/pinsstodwdevuks9h80mb/blobServices/default/containers/odw-config-db"
 }
 
 import {
   to = module.synapse_data_lake.azurerm_storage_container.odw_curated_migration
-  id = "pinsstodwdevuks9h80mb/odw-curated-migration"
+  id = "/subscriptions/ff442a29-fc06-4a13-8e3e-65fd5da513b3/resourceGroups/pins-rg-data-odw-dev-uks/providers/Microsoft.Storage/storageAccounts/pinsstodwdevuks9h80mb/blobServices/default/containers/odw-curated-migration"
 }
 
 import {
   to = module.synapse_data_lake.azurerm_storage_container.odw_standardised_delta
-  id = "pinsstodwdevuks9h80mb/odw-standardised-delta"
+  id = "/subscriptions/ff442a29-fc06-4a13-8e3e-65fd5da513b3/resourceGroups/pins-rg-data-odw-dev-uks/providers/Microsoft.Storage/storageAccounts/pinsstodwdevuks9h80mb/blobServices/default/containers/odw-standardised-delta"
 }
 
 import {
   to = module.synapse_data_lake.azurerm_storage_container.s51_advice_backup
-  id = "pinsstodwdevuks9h80mb/s51-advice-backup"
+  id = "/subscriptions/ff442a29-fc06-4a13-8e3e-65fd5da513b3/resourceGroups/pins-rg-data-odw-dev-uks/providers/Microsoft.Storage/storageAccounts/pinsstodwdevuks9h80mb/blobServices/default/containers/s51-advice-backup"
 }
 
 import {
   to = module.synapse_data_lake.azurerm_storage_container.saphrspdata_to_odw
-  id = "pinsstodwdevuks9h80mb/saphrspdata-to-odw"
+  id = "/subscriptions/ff442a29-fc06-4a13-8e3e-65fd5da513b3/resourceGroups/pins-rg-data-odw-dev-uks/providers/Microsoft.Storage/storageAccounts/pinsstodwdevuks9h80mb/blobServices/default/containers/saphrspdata-to-odw"
 }
 
 import {
   to = module.synapse_data_lake.azurerm_storage_container.synapse["synapse"]
-  id = "pinsstodwdevuks9h80mb/synapse"
+  id = "/subscriptions/ff442a29-fc06-4a13-8e3e-65fd5da513b3/resourceGroups/pins-rg-data-odw-dev-uks/providers/Microsoft.Storage/storageAccounts/pinsstodwdevuks9h80mb/blobServices/default/containers/synapse"
 }
