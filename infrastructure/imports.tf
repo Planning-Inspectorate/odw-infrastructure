fac141342fe5b596c30d0eb4205b3af416dfc3cc
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-08-28 11:45:22 +0100

    Fix logic error

--- infrastructure/imports.tf
+++ infrastructure/imports.tf
@@ -1,35 +1,35 @@
 import {
-  to = module.synapse_data_lake.azurerm_storage_container.insights_logs_builtinsqlreqsended
+  to = module.synapse_data_lake.azurerm_storage_container.synapse["insights-logs-builtinsqlreqsended"]
   id = "https://${module.synapse_data_lake.azurerm_storage_account.synapse.name}.blob.core.windows.net/insights-logs-builtinsqlreqsended"
 }
 
 import {
-  to = module.synapse_data_lake.azurerm_storage_container.logging
+  to = module.synapse_data_lake.azurerm_storage_container.synapse["logging"]
   id = "https://${module.synapse_data_lake.azurerm_storage_account.synapse.name}.blob.core.windows.net/logging"
 }
 
 import {
-  to = module.synapse_data_lake.azurerm_storage_container.odw_config_db
+  to = module.synapse_data_lake.azurerm_storage_container.synapse["odw-config-db"]
   id = "https://${module.synapse_data_lake.azurerm_storage_account.synapse.name}.blob.core.windows.net/odw-config-db"
 }
 
 import {
-  to = module.synapse_data_lake.azurerm_storage_container.odw_curated_migration
+  to = module.synapse_data_lake.azurerm_storage_container.synapse["odw-curated-migration"]
   id = "https://${module.synapse_data_lake.azurerm_storage_account.synapse.name}.blob.core.windows.net/odw-curated-migration"
 }
 
 import {
-  to = module.synapse_data_lake.azurerm_storage_container.odw_standardised_delta
+  to = module.synapse_data_lake.azurerm_storage_container.synapse["odw-standardised-delta"]
   id = "https://${module.synapse_data_lake.azurerm_storage_account.synapse.name}.blob.core.windows.net/odw-standardised-delta"
 }
 
 import {
-  to = module.synapse_data_lake.azurerm_storage_container.s51_advice_backup
+  to = module.synapse_data_lake.azurerm_storage_container.synapse["s51-advice-backup"]
   id = "https://${module.synapse_data_lake.azurerm_storage_account.synapse.name}.blob.core.windows.net/s51-advice-backup"
 }
 
 import {
-  to = module.synapse_data_lake.azurerm_storage_container.saphrspdata_to_odw
+  to = module.synapse_data_lake.azurerm_storage_container.synapse["saphrspdata-to-odw"]
   id = "https://${module.synapse_data_lake.azurerm_storage_account.synapse.name}.blob.core.windows.net/saphrspdata-to-odw"
 }
 
