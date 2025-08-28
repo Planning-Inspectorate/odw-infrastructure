cbd63da560add2defc80728c608582ff3b71cf0f
frederic jonquieres <fjonquieres83@gmail.com>
2025-08-28 10:50:34 +0100

    Updating ID format

--- infrastructure/imports.tf
+++ infrastructure/imports.tf
@@ -1,39 +1,39 @@
 import {
-  to = module.synapse_data_lake.azurerm_storage_container.insights_logs_builtinsqlreqsended
-  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/insights-logs-builtinsqlreqsended"
+  to = module.synapse_data_lake.azurerm_storage_container.logging
+  id = "pinsstodwdevuks9h80mb/logging"
 }
 
 import {
-  to = module.synapse_data_lake.azurerm_storage_container.logging
-  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/logging"
+  to = module.synapse_data_lake.azurerm_storage_container.insights_logs_builtinsqlreqsended
+  id = "pinsstodwdevuks9h80mb/insights-logs-builtinsqlreqsended"
 }
 
 import {
   to = module.synapse_data_lake.azurerm_storage_container.odw_config_db
-  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/odw-config-db"
+  id = "pinsstodwdevuks9h80mb/odw-config-db"
 }
 
 import {
   to = module.synapse_data_lake.azurerm_storage_container.odw_curated_migration
-  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/odw-curated-migration"
+  id = "pinsstodwdevuks9h80mb/odw-curated-migration"
 }
 
 import {
   to = module.synapse_data_lake.azurerm_storage_container.odw_standardised_delta
-  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/odw-standardised-delta"
+  id = "pinsstodwdevuks9h80mb/odw-standardised-delta"
 }
 
 import {
   to = module.synapse_data_lake.azurerm_storage_container.s51_advice_backup
-  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/s51-advice-backup"
+  id = "pinsstodwdevuks9h80mb/s51-advice-backup"
 }
 
 import {
   to = module.synapse_data_lake.azurerm_storage_container.saphrspdata_to_odw
-  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/saphrspdata-to-odw"
+  id = "pinsstodwdevuks9h80mb/saphrspdata-to-odw"
 }
 
 import {
   to = module.synapse_data_lake.azurerm_storage_container.synapse["synapse"]
-  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/synapse"
+  id = "pinsstodwdevuks9h80mb/synapse"
 }
