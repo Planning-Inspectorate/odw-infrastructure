7a34674463cb0dda9490c0a27a4bc4daa0234cde
frederic jonquieres <fjonquieres83@gmail.com>
2025-08-28 10:16:00 +0100

    Moving imports to the root folder and keeping resource definitions in module

---
+++ infrastructure/imports.tf
@@ -0,0 +1,39 @@
+import {
+  to = module.synapse_data_lake.azurerm_storage_container.insights_logs_builtinsqlreqsended
+  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/insights-logs-builtinsqlreqsended"
+}
+
+import {
+  to = module.synapse_data_lake.azurerm_storage_container.logging
+  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/logging"
+}
+
+import {
+  to = module.synapse_data_lake.azurerm_storage_container.odw_config_db
+  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/odw-config-db"
+}
+
+import {
+  to = module.synapse_data_lake.azurerm_storage_container.odw_curated_migration
+  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/odw-curated-migration"
+}
+
+import {
+  to = module.synapse_data_lake.azurerm_storage_container.odw_standardised_delta
+  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/odw-standardised-delta"
+}
+
+import {
+  to = module.synapse_data_lake.azurerm_storage_container.s51_advice_backup
+  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/s51-advice-backup"
+}
+
+import {
+  to = module.synapse_data_lake.azurerm_storage_container.saphrspdata_to_odw
+  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/saphrspdata-to-odw"
+}
+
+import {
+  to = module.synapse_data_lake.azurerm_storage_container.synapse
+  id = "https://pinsstodwdevuks9h80mb.blob.core.windows.net/synapse"
+}
