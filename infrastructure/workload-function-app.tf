7a7970d6f9dfb11499420e717133859c230ec266
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-10-28 10:22:53 +0000

    Add odw vnet for new storage account

--- infrastructure/workload-function-app.tf
+++ infrastructure/workload-function-app.tf
@@ -89,14 +89,15 @@ module "storage_account_openlineage" {
 
   source = "./modules/storage-account"
 
-  resource_group_name   = azurerm_resource_group.function_app[0].name
-  service_name          = local.service_name
-  environment           = var.environment
-  location              = module.azure_region.location_cli
-  tags                  = local.tags
-  network_rules_enabled = true
-  container_name        = var.openlineage_storage_account.container_name
-  tables                = var.openlineage_storage_account.tables
+  resource_group_name                     = azurerm_resource_group.function_app[0].name
+  service_name                            = local.service_name
+  environment                             = var.environment
+  location                                = module.azure_region.location_cli
+  tags                                    = local.tags
+  network_rules_enabled                   = true
+  network_rule_virtual_network_subnet_ids = concat([module.synapse_network.vnet_subnets[local.functionapp_subnet_name], module.synapse_network.vnet_subnets[local.compute_subnet_name]])
+  container_name                          = var.openlineage_storage_account.container_name
+  tables                                  = var.openlineage_storage_account.tables
 }
 
 module "function_app" {
