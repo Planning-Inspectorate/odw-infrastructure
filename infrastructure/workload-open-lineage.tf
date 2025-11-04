d942e9766c54e6048e64ec6f4cd7bbc81f54d9c2
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-11-04 10:58:25 +0000

    Improve formatting

--- infrastructure/workload-open-lineage.tf
+++ infrastructure/workload-open-lineage.tf
@@ -10,11 +10,11 @@ module "storage_account_openlineage" {
   tags                                    = local.tags
   network_rules_enabled                   = true
   network_rule_virtual_network_subnet_ids = concat([module.synapse_network.vnet_subnets[local.functionapp_subnet_name], module.synapse_network.vnet_subnets[local.compute_subnet_name]])
-  container_name                          = [
+  container_name = [
     "openlineage-events",
     "openlineage-deadletter"
   ]
-  tables                                  = [
+  tables = [
     "LineageJobs"
   ]
   shares = []
