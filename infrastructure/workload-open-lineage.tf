a8d5f2318106d15708170fd011b43b0531437fe4
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-11-06 16:17:28 +0000

    Improve styling

--- infrastructure/workload-open-lineage.tf
+++ infrastructure/workload-open-lineage.tf
@@ -4,9 +4,9 @@ data "azuread_group" "odw_data_engineers" {
 
 
 resource "azurerm_resource_group" "open_lineage_resource_group" {
-  count = var.open_lineage_enabled ? 1 : 0
+  count    = var.open_lineage_enabled ? 1 : 0
   location = module.azure_region.location_cli
-  name = "pins-rg-openlineage-odw-${var.environment}-uks"
+  name     = "pins-rg-openlineage-odw-${var.environment}-uks"
 }
 
 
@@ -61,23 +61,23 @@ module "open_lineage_receiver_service_plan" {
 
 
 module "open_lineage_receiver_function_app" {
-  count = var.open_lineage_enabled ? 1 : 0
+  count  = var.open_lineage_enabled ? 1 : 0
   source = "./modules/function-app"
 
-  resource_group_name          = azurerm_resource_group.open_lineage_resource_group[0].name
-  function_app_name            = "oljsonreceiver"
-  service_name                 = local.service_name
-  service_plan_id              = module.open_lineage_receiver_service_plan[0].id
-  storage_account_name         = module.storage_account_openlineage[0].storage_name
-  storage_account_access_key   = module.storage_account_openlineage[0].primary_access_key
-  environment                  = var.environment
-  location                     = module.azure_region.location_cli
-  tags                         = local.tags
-  application_insights_key     = azurerm_application_insights.open_lineage_insights["oljsonreceiver"].instrumentation_key
-  synapse_vnet_subnet_names    = module.synapse_network.vnet_subnets
-  app_settings                 = null
-  connection_strings           = []
-  site_config                  = {
+  resource_group_name        = azurerm_resource_group.open_lineage_resource_group[0].name
+  function_app_name          = "oljsonreceiver"
+  service_name               = local.service_name
+  service_plan_id            = module.open_lineage_receiver_service_plan[0].id
+  storage_account_name       = module.storage_account_openlineage[0].storage_name
+  storage_account_access_key = module.storage_account_openlineage[0].primary_access_key
+  environment                = var.environment
+  location                   = module.azure_region.location_cli
+  tags                       = local.tags
+  application_insights_key   = azurerm_application_insights.open_lineage_insights["oljsonreceiver"].instrumentation_key
+  synapse_vnet_subnet_names  = module.synapse_network.vnet_subnets
+  app_settings               = null
+  connection_strings         = []
+  site_config = {
     application_stack = {
       python_version = "3.11"
     }
@@ -93,14 +93,14 @@ module "open_lineage_receiver_function_app" {
 
 # Role assignments
 resource "azurerm_role_assignment" "open_lineage_storage_contributors" {
-  count = var.open_lineage_enabled ? 1 : 0
+  count                = var.open_lineage_enabled ? 1 : 0
   scope                = module.storage_account_openlineage[0].storage_id
   role_definition_name = "Contributor"
   principal_id         = data.azuread_group.odw_data_engineers.object_id
 }
 
 resource "azurerm_role_assignment" "open_lineage_storage_blob_data_contributors" {
-  count = var.open_lineage_enabled ? 1 : 0
+  count                = var.open_lineage_enabled ? 1 : 0
   scope                = module.storage_account_openlineage[0].storage_id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = data.azuread_group.odw_data_engineers.object_id
@@ -108,7 +108,7 @@ resource "azurerm_role_assignment" "open_lineage_storage_blob_data_contributors"
 
 
 resource "azurerm_role_assignment" "open_lineage_receiver_contributors" {
-  count = var.open_lineage_enabled ? 1 : 0
+  count                = var.open_lineage_enabled ? 1 : 0
   scope                = module.open_lineage_receiver_function_app[0].id
   role_definition_name = "Contributor"
   principal_id         = data.azuread_group.odw_data_engineers.object_id
