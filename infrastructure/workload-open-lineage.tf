0d8182028e42df0d12c046a8e44e9f4f8f25b8db
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-11-11 10:45:03 +0000

    Update target subnet for the open lineage private endpoint

--- infrastructure/workload-open-lineage.tf
+++ infrastructure/workload-open-lineage.tf
@@ -126,7 +126,7 @@ resource "azurerm_private_endpoint" "tooling_open_lineage_storage" {
   name                = "pins-pe-st-open-lineage-tooling-${local.resource_suffix}"
   resource_group_name = "pins-rg-network-odw-${var.environment}-uks"
   location            = module.azure_region.location_cli
-  subnet_id           = module.synapse_network.vnet_subnets["SynapseEndpointSubnet"]
+  subnet_id           = module.synapse_network.vnet_subnets["ComputeSubnet"]
 
   private_dns_zone_group {
     name = "storageOpenlineagePrivateDnsZone"
