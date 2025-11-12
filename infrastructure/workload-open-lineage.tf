fd0b7fa2fe8c16816fea5fb5eed941fb597deeff
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-11-12 09:30:09 +0000

    Add role assignment for OL function app to access OL storage

--- infrastructure/workload-open-lineage.tf
+++ infrastructure/workload-open-lineage.tf
@@ -131,6 +131,14 @@ resource "azurerm_role_assignment" "open_lineage_function_app_contributors" {
   principal_id         = each.value[1]
 }
 
+resource "azurerm_role_assignment" "open_lineage_function_app_storage_contributors" {
+  for_each             = azurerm_linux_function_app.open_lineage_function_app
+  scope                = module.storage_account_openlineage[0].storage_id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = each.value.identity[0].principal_id
+}
+
+
 
 # Private endpoints
 resource "azurerm_private_endpoint" "tooling_open_lineage_storage" {
