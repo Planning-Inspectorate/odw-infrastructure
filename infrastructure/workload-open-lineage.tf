d92e3e10d3684f3ff3463b3de83b2a482010c98e
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-11-12 11:44:37 +0000

    Add CORS block

--- infrastructure/workload-open-lineage.tf
+++ infrastructure/workload-open-lineage.tf
@@ -100,7 +100,7 @@ resource "azurerm_linux_function_app" "open_lineage_function_app" {
       python_version = "3.11"
     }
     cors {
-      allowed_origins     = "https://portal.azure.com"
+      allowed_origins     = ["https://portal.azure.com"]
       support_credentials = false
     }
   }
