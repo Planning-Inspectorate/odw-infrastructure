fb5ab3c5195507b9a12b40eaabe76511f7daad03
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2025-11-13 17:04:40 +0000

    Remove unvalid app setting

--- infrastructure/workload-open-lineage.tf
+++ infrastructure/workload-open-lineage.tf
@@ -86,8 +86,7 @@ resource "azurerm_linux_function_app" "open_lineage_function_app" {
     "ALLOW_SQL_ONLY"                           = "true",
     "MAX_CONTENT_LENGTH"                       = 8388608,
     "WRITE_TABLE"                              = "true",
-    "PURVIEW_NAME"                             = "pins-pview",
-    "CLIENT_ID"                                = azurerm_linux_function_app.open_lineage_function_app.identity[0].principal_id
+    "PURVIEW_NAME"                             = "pins-pview"
   }
 
   site_config {
