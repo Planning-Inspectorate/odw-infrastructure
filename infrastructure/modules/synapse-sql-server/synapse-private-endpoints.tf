6c412a569193736577ba796d5534e5dbfd84ed21
Abbas M <abbas.merali@hmcts.net>
2025-12-18 11:38:00 +0000

    adding private endpoint for cbos sql

--- infrastructure/modules/synapse-sql-server/synapse-private-endpoints.tf
+++ infrastructure/modules/synapse-sql-server/synapse-private-endpoints.tf
@@ -4,3 +4,10 @@ resource "azurerm_synapse_managed_private_endpoint" "data_lake" {
   target_resource_id   = azurerm_mssql_server.sql_server.id
   subresource_name     = "sqlServer"
 }
+
+resource "azurerm_synapse_managed_private_endpoint" "cbos_sql_dev" {
+  name                 = "synapse-cbos-sqlServer-dev"
+  synapse_workspace_id = var.synapse_workspace_id
+  target_resource_id   = "/subscriptions/962e477c-0f3b-4372-97fc-a198a58e259e/resourceGroups/pins-rg-local-plans-dev/providers/Microsoft.Sql/servers/pins-sql-local-plans-primary-dev"
+  subresource_name     = "sqlServer"
+}
