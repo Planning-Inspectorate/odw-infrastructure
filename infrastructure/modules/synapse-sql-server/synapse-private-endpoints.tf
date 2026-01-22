6a822514c232d85491232ad7be418ba8432abffb
Abbas M <abbas.merali@hmcts.net>
2026-01-22 11:12:48 +0000

    DEV private endpoint

--- infrastructure/modules/synapse-sql-server/synapse-private-endpoints.tf
+++ infrastructure/modules/synapse-sql-server/synapse-private-endpoints.tf
@@ -6,6 +6,7 @@ resource "azurerm_synapse_managed_private_endpoint" "data_lake" {
 }
 
 resource "azurerm_synapse_managed_private_endpoint" "cbos_sql_dev" {
+  count = var.environment == "dev" ? 1 : 0
   name                 = "synapse-cbos-sqlserver-dev-peas"
   synapse_workspace_id = var.synapse_workspace_id
   target_resource_id   = "/subscriptions/962e477c-0f3b-4372-97fc-a198a58e259e/resourceGroups/pins-rg-peas-dev/providers/Microsoft.Sql/servers/pins-sql-peas-primary-dev"
