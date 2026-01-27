64f1c1fb5e3f28a4245e8a0e42c2a91fca55115f
EvangelosMagklaras <evangelos.magklaras@planninginspectorate.gov.uk>
2026-01-27 15:20:18 +0000

    feat: create New Storrage Account

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -169,6 +169,14 @@ variable "horizon_integration_config" {
   })
 }
 
+#Evangelos new variables Horizon migration
+variable "horizon_migration" {
+  description = "Configuration for Horizon data migration"
+  type = object({
+    rg           = string
+    service_name = string
+  })
+}
 variable "key_vault_role_assignments" {
   default     = {}
   description = "An object mapping RBAC roles to principal IDs for Key Vault"
