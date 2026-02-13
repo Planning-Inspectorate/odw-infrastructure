6b05ac35802cdb1b53987c3698c21b386ebc565f
EvangelosMagklaras <evangelos.magklaras@planninginspectorate.gov.uk>
2026-02-13 17:08:42 +0000

    adding network ids for mpsec conatiner

--- infrastructure/workload-mpesc.tf
+++ infrastructure/workload-mpesc.tf
@@ -5,12 +5,13 @@ module "storage_account_horizon_migration" {
 
   source = "./modules/storage-account"
 
-  resource_group_name = var.horizon_migration.rg
-  service_name        = var.horizon_migration.service_name
-  environment         = var.environment
-  location            = module.azure_region.location_cli
-  tags                = local.tags
-  container_name      = var.horizon_migration.container_name
+  resource_group_name                     = var.horizon_migration.rg
+  service_name                            = var.horizon_migration.service_name
+  environment                             = var.environment
+  location                                = module.azure_region.location_cli
+  tags                                    = local.tags
+  container_name                          = var.horizon_migration.container_name
+  network_rule_virtual_network_subnet_ids = concat([module.synapse_network.vnet_subnets[local.functionapp_subnet_name], module.synapse_network.vnet_subnets[local.compute_subnet_name]])
 }
 
 
