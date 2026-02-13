8fda9cd3197a926e2d8a1f1e44bd5ad656e719b1
EvangelosMagklaras <53814173+EvangelosMagklaras@users.noreply.github.com>
2026-02-13 16:47:35 +0000

    Add network rule for virtual network subnets

--- infrastructure/workload-mpesc.tf
+++ infrastructure/workload-mpesc.tf
@@ -10,6 +10,7 @@ module "storage_account_horizon_migration" {
   environment         = var.environment
   location            = module.azure_region.location_cli
   tags                = local.tags
+network_rule_virtual_network_subnet_ids = concat([module.synapse_network.vnet_subnets[local.functionapp_subnet_name], module.synapse_network.vnet_subnets[local.compute_subnet_name]])
   container_name      = var.horizon_migration.container_name
 }
 
