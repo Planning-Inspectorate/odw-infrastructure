cd99dd21df32baf4d525ff7796cd24661d64b7cb
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-02-25 11:02:33 +0000

    Remove redundant appeals and backoffice private endpoints. Remove redundant DNS zone

--- infrastructure/workload-odt-backoffice-sb.tf
+++ infrastructure/workload-odt-backoffice-sb.tf
@@ -16,52 +16,18 @@ resource "azurerm_resource_group" "odt_backoffice_sb_failover" {
   tags = local.tags
 }
 
-resource "azurerm_resource_group" "odt_backoffice_sb_global" {
-  count = var.odt_back_office_service_bus_enabled ? 1 : 0
-
-  name     = "pins-rg-odt-bo-sb-${local.resource_suffix_global}"
-  location = module.azure_region.location_cli
-
-  tags = local.tags
-}
-
-resource "azurerm_private_dns_zone" "back_office_private_dns_zone" {
-  count = var.odt_back_office_service_bus_enabled && var.environment != "dev" ? 1 : 0
-
-  name                = "privatelink.servicebus.windows.net"
-  resource_group_name = azurerm_resource_group.odt_backoffice_sb_global[0].name
-
-  tags = local.tags
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "back_office_private_dns_zone_vnet_link" {
-  count = var.odt_back_office_service_bus_enabled && var.environment != "dev" ? 1 : 0
-
-  name                  = "pins-pdns-vnet-link-backoffice-sb-${local.resource_suffix}"
-  resource_group_name   = azurerm_resource_group.odt_backoffice_sb_global[0].name
-  private_dns_zone_name = azurerm_private_dns_zone.back_office_private_dns_zone[0].name
-  virtual_network_id    = module.synapse_network.vnet_id
-  registration_enabled  = false
-
-  tags = local.tags
-}
-
 module "odt_backoffice_sb" {
   count = var.odt_back_office_service_bus_enabled && var.external_resource_links_enabled ? 1 : 0
 
   source = "./modules/odt-backoffice-sb"
 
-  environment                                  = var.environment
-  resource_group_name                          = azurerm_resource_group.odt_backoffice_sb[0].name
-  location                                     = module.azure_region.location_cli
-  service_name                                 = local.service_name
-  odt_backoffice_sb_topic_subscriptions        = var.odt_backoffice_sb_topic_subscriptions
-  odt_back_office_service_bus_id               = local.odt_back_office_service_bus_id
-  odt_back_office_private_endpoint_dns_zone_id = (var.environment != "dev" ? azurerm_private_dns_zone.back_office_private_dns_zone[0].id : null)
-  synapse_private_endpoint_subnet_name         = local.synapse_subnet_name
-  synapse_private_endpoint_vnet_subnets        = module.synapse_network.vnet_subnets
-  synapse_workspace_failover_principal_id      = try(module.synapse_workspace_private_failover.synapse_workspace_principal_id, null)
-  synapse_workspace_principal_id               = module.synapse_workspace_private.synapse_workspace_principal_id
+  environment                             = var.environment
+  location                                = module.azure_region.location_cli
+  service_name                            = local.service_name
+  odt_backoffice_sb_topic_subscriptions   = var.odt_backoffice_sb_topic_subscriptions
+  odt_back_office_service_bus_id          = local.odt_back_office_service_bus_id
+  synapse_workspace_failover_principal_id = try(module.synapse_workspace_private_failover.synapse_workspace_principal_id, null)
+  synapse_workspace_principal_id          = module.synapse_workspace_private.synapse_workspace_principal_id
 
   tags = local.tags
 
@@ -76,17 +42,13 @@ module "odt_backoffice_sb_failover" {
 
   source = "./modules/odt-backoffice-sb"
 
-  environment                                  = var.environment
-  resource_group_name                          = azurerm_resource_group.odt_backoffice_sb_failover[0].name
-  location                                     = module.azure_region.location_cli
-  service_name                                 = local.service_name
-  odt_backoffice_sb_topic_subscriptions        = var.odt_backoffice_sb_topic_subscriptions
-  odt_back_office_service_bus_id               = local.odt_back_office_service_bus_id
-  odt_back_office_private_endpoint_dns_zone_id = (var.environment != "dev" ? azurerm_private_dns_zone.back_office_private_dns_zone[0].id : null)
-  synapse_private_endpoint_subnet_name         = local.synapse_subnet_name
-  synapse_private_endpoint_vnet_subnets        = module.synapse_network_failover.vnet_subnets
-  synapse_workspace_failover_principal_id      = try(module.synapse_workspace_private_failover.synapse_workspace_principal_id, null)
-  synapse_workspace_principal_id               = module.synapse_workspace_private.synapse_workspace_principal_id
+  environment                             = var.environment
+  location                                = module.azure_region.location_cli
+  service_name                            = local.service_name
+  odt_backoffice_sb_topic_subscriptions   = var.odt_backoffice_sb_topic_subscriptions
+  odt_back_office_service_bus_id          = local.odt_back_office_service_bus_id
+  synapse_workspace_failover_principal_id = try(module.synapse_workspace_private_failover.synapse_workspace_principal_id, null)
+  synapse_workspace_principal_id          = module.synapse_workspace_private.synapse_workspace_principal_id
 
   tags = local.tags
 
@@ -102,19 +64,15 @@ module "odt_appeals_back_office_sb" {
 
   source = "./modules/odt-backoffice-sb"
 
-  back_office_name                             = "appeals-backoffice"
-  environment                                  = var.environment
-  resource_group_name                          = azurerm_resource_group.odt_backoffice_sb[0].name
-  location                                     = module.azure_region.location_cli
-  service_name                                 = local.service_name
-  odt_backoffice_sb_topic_subscriptions        = var.odt_appeals_back_office_sb_topic_subscriptions
-  odt_back_office_service_bus_id               = local.odt_appeals_back_office_service_bus_id
-  odt_back_office_private_endpoint_dns_zone_id = (var.environment != "dev" ? azurerm_private_dns_zone.back_office_private_dns_zone[0].id : null)
-  synapse_private_endpoint_subnet_name         = local.synapse_subnet_name
-  synapse_private_endpoint_vnet_subnets        = module.synapse_network.vnet_subnets
-  synapse_workspace_failover_principal_id      = try(module.synapse_workspace_private_failover.synapse_workspace_principal_id, null)
-  synapse_workspace_principal_id               = module.synapse_workspace_private.synapse_workspace_principal_id
-  topics_to_send                               = ["listed-building"]
+  back_office_name                        = "appeals-backoffice"
+  environment                             = var.environment
+  location                                = module.azure_region.location_cli
+  service_name                            = local.service_name
+  odt_backoffice_sb_topic_subscriptions   = var.odt_appeals_back_office_sb_topic_subscriptions
+  odt_back_office_service_bus_id          = local.odt_appeals_back_office_service_bus_id
+  synapse_workspace_failover_principal_id = try(module.synapse_workspace_private_failover.synapse_workspace_principal_id, null)
+  synapse_workspace_principal_id          = module.synapse_workspace_private.synapse_workspace_principal_id
+  topics_to_send                          = ["listed-building"]
 
   tags = local.tags
 
