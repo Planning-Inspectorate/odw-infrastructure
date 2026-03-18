a787507d7d17f0d4a89597f6ca7b4aa6d622da3a
Abbas M <abbas.merali@hmcts.net>
2026-03-18 13:12:32 +0000

    adding endpoint only for prod

--- infrastructure/modules/synapse-sql-server/synapse-private-endpoints.tf
+++ infrastructure/modules/synapse-sql-server/synapse-private-endpoints.tf
@@ -7,6 +7,8 @@ resource "azurerm_synapse_managed_private_endpoint" "data_lake" {
 
 
 resource "azurerm_synapse_managed_private_endpoint" "mpesc_prod_sql" {
+  count = var.environment == "prod" ? 1 : 0
+
   name                 = "synapse-sql-sqlServer--pins-sql-peas-primary-prod"
   synapse_workspace_id = var.synapse_workspace_id
   target_resource_id   = "/subscriptions/d1d6c393-2fe3-40af-ac27-f5b6bad36735/resourceGroups/pins-rg-peas-prod/providers/Microsoft.Sql/servers/pins-sql-peas-primary-prod"
