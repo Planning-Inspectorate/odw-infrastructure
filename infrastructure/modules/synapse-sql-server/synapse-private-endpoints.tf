6d14ab7b5ed8fc3257057e52ccd37cc7c2cdd7ec
Abbas M <abbas.merali@hmcts.net>
2026-03-23 16:16:12 +0000

    prod endpoint

--- infrastructure/modules/synapse-sql-server/synapse-private-endpoints.tf
+++ infrastructure/modules/synapse-sql-server/synapse-private-endpoints.tf
@@ -6,10 +6,8 @@ resource "azurerm_synapse_managed_private_endpoint" "data_lake" {
 }
 
 resource "azurerm_synapse_managed_private_endpoint" "mpesc_prod_sql" {
-  count = var.environment == "prod" ? 1 : 0
-
   name                 = "synapse-sql-sqlServer--pins-sql-peas-primary-prod"
-  synapse_workspace_id = var.synapse_workspace_id
+  synapse_workspace_id = "/subscriptions/a82fd28d-5989-4e06-a0bb-1a5d859f9e0c/resourceGroups/pins-rg-data-odw-prod-uks/providers/Microsoft.Synapse/workspaces/pins-synw-odw-prod-uks"
   target_resource_id   = "/subscriptions/d1d6c393-2fe3-40af-ac27-f5b6bad36735/resourceGroups/pins-rg-peas-prod/providers/Microsoft.Sql/servers/pins-sql-peas-primary-prod"
   subresource_name     = "sqlServer"
 }
