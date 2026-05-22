7ec7c55b2c32a0e714ea9eccf4077e6a5f334e45
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-05-22 12:55:57 +0100

    Update incorrect resource group

--- infrastructure/workload-network.tf
+++ infrastructure/workload-network.tf
@@ -67,7 +67,7 @@ resource "azurerm_private_dns_zone" "synapse" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "datalake" {
   name                  = "dfs-${module.synapse_network.vnet_name}"
-  resource_group_name   = azurerm_resource_group.network_global.name
+  resource_group_name   = var.tooling_config.network_rg
   private_dns_zone_name = data.azurerm_private_dns_zone.tooling_storage["dfs"].name
   virtual_network_id    = module.synapse_network.vnet_id
   provider              = azurerm.tooling
@@ -77,7 +77,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "datalake" {
 
 resource "azurerm_private_dns_zone_virtual_network_link" "datalake_failover" {
   name                  = "dfs-${module.synapse_network_failover.vnet_name}"
-  resource_group_name   = azurerm_resource_group.network_global.name
+  resource_group_name   = var.tooling_config.network_rg
   private_dns_zone_name = data.azurerm_private_dns_zone.tooling_storage["dfs"].name
   virtual_network_id    = module.synapse_network_failover.vnet_id
   provider              = azurerm.tooling
