a4b49ec3ce635e62de316628202b494769d02bd7
Mukundarao Gummadi <Mukundarao.Gummadi.f4@planninginspectorate.gov.uk>
2026-06-04 14:09:40 +0100

    Updated private endpoint test code

--- tests/smoke_test/test_smoke_private_endpoints.py
+++ tests/smoke_test/test_smoke_private_endpoints.py
@@ -96,7 +96,6 @@ class TestSmokePrivateEndpoints(TestCase):
     @pytest.mark.parametrize(
         "endpoint_name",
         [
-            f"pins-pe-pinskvsynwodw{ENV}uks"
             f"pins-pe-pinskvsynwodw{ENV}uks-tooling"
         ]
     )
@@ -108,7 +107,6 @@ class TestSmokePrivateEndpoints(TestCase):
         "endpoint_name",
         [
             f"pins-pe-pinskvmgmtodw{ENV}uks"
-            f"pins-pe-pinskvmgmtodw{ENV}uks-tooling"
         ]
     )
     def test_management_keyvault_private_endpoints(self, endpoint_name: str):
