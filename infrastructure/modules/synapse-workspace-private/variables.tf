1e88d9556114362d27286dfd05f8f48f30b916f6
Abbas M <abbas.merali@hmcts.net>
2026-06-22 12:57:17 +0100

    Add S62A migration storage account and container across environments

--- infrastructure/modules/synapse-workspace-private/variables.tf
+++ infrastructure/modules/synapse-workspace-private/variables.tf
@@ -277,3 +277,13 @@ variable "tooling_config" {
     synapse_sql_private_dns_zone_id = string
   })
 }
+
+variable "s62a_storage_account_name" {
+  type    = string
+  default = null
+}
+
+variable "s62a_storage_account_id" {
+  type    = string
+  default = null
+}
