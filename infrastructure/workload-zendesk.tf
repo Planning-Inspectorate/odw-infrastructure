32be1c322ed11915217a090991a939a469c2cbb5
HarrisonBoyleThomas <harrison.boyle-thomas@solirius.com>
2026-07-07 11:18:32 +0100

    Migrate zendesk resources to its own module

--- infrastructure/workload-zendesk.tf
+++ infrastructure/workload-zendesk.tf
@@ -1,5 +1,5 @@
-resource "azurerm_resource_group" "logic_app" {
-  count = var.logic_app_enabled ? 1 : 0
+resource "azurerm_resource_group" "zendesk" {
+  count = var.zendesk_enabled ? 1 : 0
 
   name     = "pins-rg-logic-app-${local.resource_suffix}"
   location = module.azure_region.location_cli
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "logic_app" {
   tags = local.tags
 }
 
-resource "azurerm_resource_group" "logic_app_failover" {
+resource "azurerm_resource_group" "zendesk_failover" {
   count = var.failover_deployment ? 1 : 0
 
   name     = "pins-rg-logic-app-${local.resource_suffix_failover}"
@@ -16,16 +16,16 @@ resource "azurerm_resource_group" "logic_app_failover" {
   tags = local.tags
 }
 
-module "logic_app" {
-  count = var.logic_app_enabled ? 1 : 0
+module "zendesk" {
+  count = var.zendesk_enabled ? 1 : 0
 
-  source = "./modules/logic-app"
+  source = "./modules/zendesk"
 
   environment                           = var.environment
-  resource_group_name                   = azurerm_resource_group.logic_app[0].name
-  resource_group_id                     = azurerm_resource_group.logic_app[0].id
+  resource_group_name                   = azurerm_resource_group.zendesk[0].name
+  resource_group_id                     = azurerm_resource_group.zendesk[0].id
   location                              = module.azure_region.location_cli
-  logic_app_enabled                     = var.logic_app_enabled
+  logic_app_enabled                     = var.zendesk_enabled
   key_vault_id                          = module.synapse_data_lake.key_vault_id
   service_name                          = local.service_name
   service_bus_primary_connection_string = module.synapse_ingestion.service_bus_primary_connection_string
@@ -33,16 +33,16 @@ module "logic_app" {
   tags = local.tags
 }
 
-module "logic_app_failover" {
-  count = var.logic_app_enabled && var.failover_deployment ? 1 : 0
+module "zendesk_failover" {
+  count = var.zendesk_enabled && var.failover_deployment ? 1 : 0
 
-  source = "./modules/logic-app"
+  source = "./modules/zendesk"
 
   environment                           = var.environment
-  resource_group_name                   = azurerm_resource_group.logic_app_failover[0].name
-  resource_group_id                     = azurerm_resource_group.logic_app_failover[0].id
+  resource_group_name                   = azurerm_resource_group.zendesk_failover[0].name
+  resource_group_id                     = azurerm_resource_group.zendesk_failover[0].id
   location                              = module.azure_region.paired_location.location_cli
-  logic_app_enabled                     = var.logic_app_enabled
+  logic_app_enabled                     = var.zendesk_enabled
   key_vault_id                          = module.synapse_data_lake.key_vault_id
   service_name                          = local.service_name
   service_bus_primary_connection_string = module.synapse_ingestion_failover[0].service_bus_primary_connection_string
