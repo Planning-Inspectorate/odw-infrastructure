17bf415ade502f71a75562ed41886cc3c7df42aa
Harrison <55688012+HarrisonBoyleThomas@users.noreply.github.com>
2026-07-10 09:31:35 +0100

    Fix/theodw 3339 migrate to tf (#192)
    * Migrate zendesk resources to their own module
    
    * Import logic app
    
    * Import data lake role assignment
    
    * Insert variables during TF plan
    
    * Improve description

---
+++ infrastructure/modules/logic-app/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_logic_app_workflow" "main" {
+  name                = var.logic_app_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  parameters          = var.parameters
+  workflow_parameters = var.workflow_parameters
+  identity {
+    type = "SystemAssigned"
+  }
+  tags = var.tags
+}
+
+resource "azurerm_logic_app_action_custom" "main" {
+  for_each     = { for elem in var.custom_actions : elem.name => elem }
+  name         = each.value.name
+  logic_app_id = each.value.logic_app_id
+  body         = each.value.body
+}
+
+resource "azurerm_logic_app_trigger_http_request" "main" {
+  for_each     = { for elem in var.http_triggers : elem.name => elem }
+  name         = each.value.name
+  method       = each.value.method
+  logic_app_id = each.value.logic_app_id
+  schema       = each.value.schema
+}
---
+++ infrastructure/modules/logic-app/variables.tf
@@ -0,0 +1,49 @@
+variable "logic_app_name" {
+  description = "The name of the logic app"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group the logic app should belong to"
+  type        = string
+}
+
+variable "location" {
+  description = "The short-format Azure region into which resources will be deployed"
+  type        = string
+}
+
+variable "parameters" {
+  description = "Parameters for the logic app. Note this is different to workflow_parameters"
+  type        = map(string)
+}
+
+variable "workflow_parameters" {
+  description = "Workflow parameters for the logic app. Note this is different to parameters"
+  type        = map(string)
+}
+
+variable "custom_actions" {
+  type = list(object({
+    name         = string
+    logic_app_id = string
+    body         = string
+  }))
+  default = []
+}
+
+variable "http_triggers" {
+  type = list(object({
+    name         = string
+    logic_app_id = string
+    method       = string
+    schema       = string
+  }))
+  default = []
+}
+
+variable "tags" {
+  description = "List of tags to be applied to resources"
+  type        = map(string)
+  default     = {}
+}
