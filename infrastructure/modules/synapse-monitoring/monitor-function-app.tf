3987abfdb73b592bac9047cf75b5de377f462ac4
Mukundarao Gummadi <Mukundarao.Gummadi.f4@planninginspectorate.gov.uk>
2026-07-21 11:32:16 +0100

    Re-enable the FunctionAppLogs diagnostic category - 3439

--- infrastructure/modules/synapse-monitoring/monitor-function-app.tf
+++ infrastructure/modules/synapse-monitoring/monitor-function-app.tf
@@ -5,6 +5,9 @@ resource "azurerm_monitor_diagnostic_setting" "function_app" {
   target_resource_id         = each.value
   log_analytics_workspace_id = azurerm_log_analytics_workspace.synapse.id
 
+  enabled_log {
+    category = "FunctionAppLogs"
+  }
 
   metric {
     category = "AllMetrics"
