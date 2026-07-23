35bceb5d3e7cddb211d87c75e50eb10fa69a95e2
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-07-23 16:18:57 +0100

    chore: consolidate function_app configuration

---
+++ infrastructure/workload-function-app-config.tf
@@ -0,0 +1,25 @@
+locals {
+  function_app = [
+    {
+      name = "fnapp01"
+      connection_strings = [
+        {
+          name  = "SqlConnectionString",
+          type  = "SQLAzure",
+          value = "Server=tcp:pins-synw-odw-${var.environment}-uks-ondemand.sql.azuresynapse.net,1433;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Database=odw_curated_db;Authentication=Active Directory Managed Identity;"
+        },
+        {
+          name  = "SqlConnectionString2",
+          type  = "SQLAzure",
+          value = "Server=tcp:pins-synw-odw-${var.environment}-uks-ondemand.sql.azuresynapse.net,1433;Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Database=odw_harmonised_db;Authentication=Active Directory Managed Identity;"
+        }
+
+      ]
+      site_config = {
+        application_stack = {
+          python_version = "3.11"
+        }
+      }
+    }
+  ]
+}
