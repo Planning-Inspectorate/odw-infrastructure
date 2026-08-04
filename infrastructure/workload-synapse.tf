1fc4116a38f1f3461b8e173d41b0f596f22baa0c
Evangelos Magklaras <evangelos.magklaras@company.com>
2026-08-04 14:13:48 +0100

    Added-odt_subscription_id

--- infrastructure/workload-synapse.tf
+++ infrastructure/workload-synapse.tf
@@ -2,6 +2,7 @@ module "synapse_workspace_private" {
   source = "./modules/synapse-workspace-private"
 
   environment         = var.environment
+  odt_subscription_id = var.odt_subscription_id
   resource_group_name = azurerm_resource_group.data.name
   location            = module.azure_region.location_cli
   service_name        = local.service_name
