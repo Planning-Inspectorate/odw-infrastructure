bc3637494f882d074f25708e2ef1513e050309e8
Evangelos Magklaras <evangelos.magklaras@company.com>
2026-09-14 15:47:33 +0100

    count condition for build envr

--- infrastructure/modules/synapse-workspace-private/synapse-private-endpoints.tf
+++ infrastructure/modules/synapse-workspace-private/synapse-private-endpoints.tf
@@ -302,7 +302,7 @@ resource "azurerm_synapse_managed_private_endpoint" "mpesc_prod_sql" {
 }
 
 # Managed private endpoint to Crown SQL server used for S62A migration
-resource "azurerm_synapse_managed_private_endpoint" "crown_sql" {
+resource "azurerm_synapse_managed_private_endpoint" "crown_sql" {count = var.environment == "build" ? 0 : 1
   name                 = "synapse-sql-sqlServer--pins-sql-crown-primary-${var.environment}"
   synapse_workspace_id = azurerm_synapse_workspace.synapse.id
 
