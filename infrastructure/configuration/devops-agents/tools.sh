fbc38360593109cb352ffdc4810cf62afb5617fa
Mukundarao Gummadi <Mukundarao.Gummadi.f4@planninginspectorate.gov.uk>
2026-09-16 11:41:23 +0100

    Rebased with main branch

--- infrastructure/configuration/devops-agents/tools.sh
+++ infrastructure/configuration/devops-agents/tools.sh
@@ -50,7 +50,7 @@ sudo apt-get install -y --no-install-recommends \
   python3.11-distutils \
   python3.11-venv
 
-# Adding python3.11 must not disturb cloud-init's own python3.10 + PyYAML dependency
+# Adding python3.11 must not disturb cloud-init's own python3.10 + PyYAML dependency 
 sudo apt-get install -y --reinstall python3-yaml
 
 sudo ln -sf /usr/bin/python3.11 /usr/local/bin/python3
