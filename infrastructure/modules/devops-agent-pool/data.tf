8d4dade24e599e2a8042af0f89d3b24649f70b85
Mukundarao Gummadi <Mukundarao.Gummadi.f4@planninginspectorate.gov.uk>
2026-09-17 16:41:41 +0100

    Added working dev image

--- infrastructure/modules/devops-agent-pool/data.tf
+++ infrastructure/modules/devops-agent-pool/data.tf
@@ -1,6 +1,6 @@
 data "azurerm_image" "azure_agents" {
   count = var.deploy_agent_pool ? 1 : 0
 
-  name                = "devops-agents-20260917100735"
+  name                = "devops-agents-20260824131442"
   resource_group_name = azurerm_resource_group.devops_agents.name
 }
