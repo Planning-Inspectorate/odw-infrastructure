b755bcb55b1dfc3647e8700d07aaf3e90291e4ca
Mukundarao Gummadi <Mukundarao.Gummadi.f4@planninginspectorate.gov.uk>
2026-09-17 15:21:07 +0100

    Python 3.11 code is added

--- infrastructure/configuration/devops-agents/tools.sh
+++ infrastructure/configuration/devops-agents/tools.sh
@@ -54,9 +54,13 @@ sudo apt install -y --no-install-recommends \
 
 # Python 3.11
 sudo apt-get install -y --no-install-recommends \
-  python3 \
-  python3-distutils \
-  python3-pip
+  python3.11 \
+  python3.11-distutils \
+  python3.11-venv
+
+sudo ln -sf /usr/bin/python3.11 /usr/local/bin/python3
+curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python3
+python3 --version | grep -q '^Python 3\.11\.'
 
 
 # Python dependencies
